93e778271a10f5151d5b8406767f9b71387b1955
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2023-08-14 15:33:57 +0700

    collapsible navbar on narrow screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,20 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+
+  const [isNavExpanded, setIsNavExpanded] = React.useState(false)
+
+  function toggleNav() {
+    setIsNavExpanded(!isNavExpanded)
+  }
+
   return (
     <header>
-      <Link to="/" className="site-logo">GM</Link>
-      <nav>
+      <div className="header-top">
+        <Link to="/" className="site-logo">GM</Link>
+        <button class="hamburger-btn" onClick={toggleNav}><i class="fa-solid fa-bars fa-xl"></i></button>
+      </div>
+      <nav className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
         <NavLink to="/portfolio">Portfolio</NavLink>
         <NavLink to="/blog">Blog</NavLink>
         <NavLink to="/contact">Contact</NavLink>
